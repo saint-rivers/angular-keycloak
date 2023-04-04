8f0d9a964f1d59bcf3cf7b56a7e78a18de653f56
dayan <eam.dayan@gmail.com>
2023-04-04 16:20:53 +0700

    init

---
+++ src/app/components/login-button/login-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-button',
+  templateUrl: './login-button.component.html',
+  styleUrls: ['./login-button.component.css'],
+})
+export class LoginButtonComponent {
+  handleLogin() {
+    alert('log');
+  }
+}
